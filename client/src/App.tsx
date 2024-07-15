5f7b0fe7255c87fe02563cb94583f7b59d476745
alexjimenez-dosespot <alex.jimenez@dosespot.com>
2024-07-15 16:18:14 -0400

    initial assessment

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+
+export const App: React.FC = () => {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to DoseSpot</h1>
+        </header>
+        
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <footer>
+        </footer>
+      </div>
+    );
+};
